4d7525b204c66e27c38bed2c84422920a0fb6a12
knapkejp <jknapke17@gmail.com>
2020-10-06 17:39:00 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    Student student1 = new Student();
+
+    student1.setName("Arnold Spielburg");
+    student1.setGPA(4.0);
+    student1.setMajor("Electrical Engineering");
+  }
+}
---
+++ Student.java
@@ -0,0 +1,30 @@
+class Student {
+  String name;
+  double gpa;
+  String major;
+
+  String getName(){
+    return name;
+  }
+
+  double getGPA {
+    return gpa;
+  }
+
+  String getmajor(){
+    return major;
+  }
+
+  void setName(String theName) {
+    name = theName;
+  }
+
+  void setGPA(String theGPA) {
+    gpa = theGPA;
+  }
+
+  void setMajor(String theMajor) {
+    major = theMajor;
+  }
+
+}
